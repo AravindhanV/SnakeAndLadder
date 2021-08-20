e7a743e8790e1bae69560b948dd13c92f00b344d
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 16:54:20 +0530

    ADDED: Initial Position for Player

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder Simulator");
+		
+		int playerPos = 0;
 	}
 }
