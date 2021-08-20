52d0fa2019b2d26b899b8c44b91becf90d282659
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 18:00:59 +0530

    MODIFIED: Now a 2-player Game

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,24 +4,31 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder Simulator");
 		
-		int playerPos = 0;
-		int noOfTurns = 0;
+		int[] playerPos = {0,0};
+		int turnFlag = 0;
+		int[] noOfTurns = {0,0};
 		//Roll a Die
-		while(playerPos<100) {
+		while(playerPos[0]<100 && playerPos[1]<100) {
 			int dieRoll = (int) Math.floor(Math.random()*6)+1;
 			int action = (int)Math.floor(Math.random()*3);
+			noOfTurns[turnFlag]++;
 			switch(action) {
-			case 0: break; //Do Nothing
-			case 1: playerPos += dieRoll; //Ladder
-			playerPos = playerPos>100 ? playerPos-dieRoll : playerPos;
+			case 0: break; //No Move
+			case 1: playerPos[turnFlag] += dieRoll; //Ladder
+			playerPos[turnFlag] = playerPos[turnFlag]>100 ? playerPos[turnFlag]-dieRoll : playerPos[turnFlag];
 			break;
-			case 2: playerPos -= dieRoll; //Snake
-			playerPos = playerPos<0 ? 0 : playerPos;
+			case 2: playerPos[turnFlag] -= dieRoll; //Snake
+			playerPos[turnFlag] = playerPos[turnFlag]<0 ? 0 : playerPos[turnFlag];
 			break;
 			}
-			noOfTurns++;
-			System.out.println("Player is now at "+playerPos);
+			System.out.println("Player "+(turnFlag+1)+" is now at "+playerPos[turnFlag]);
+			
+			if(action == 0 || action == 2) {
+				turnFlag = (turnFlag+1)%2; 
+			}
 		}
-		System.out.println("\nPlayer took "+noOfTurns+" turns to complete the game");
+		
+		int winner = playerPos[0] == 100 ? 1 : 2;
+		System.out.println("\nPlayer "+winner+" won the game in "+noOfTurns[winner-1]+" turns");
 	}
 }
